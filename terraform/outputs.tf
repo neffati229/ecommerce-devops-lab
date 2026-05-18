e1c2317d0147e06127454ed17d2e00371834a767
neffati229 <hajer@neffati229.com>
2026-05-18 20:27:40 +0100

    Fix outputs.tf for web1 and web2 instances

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,17 @@
 output "instance_public_ips" {
   description = "Public IPs of EC2 instances"
-  value       = aws_instance.web[*].public_ip
+  value = [
+    aws_instance.web1.public_ip,
+    aws_instance.web2.public_ip
+  ]
 }
 
-output "instance_public_ip" {
+output "instance_public_ip_1" {
   description = "Public IP of first EC2 instance"
-  value       = aws_instance.web[0].public_ip
+  value = aws_instance.web1.public_ip
+}
+
+output "instance_public_ip_2" {
+  description = "Public IP of second EC2 instance"
+  value = aws_instance.web2.public_ip
 }
