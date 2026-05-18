69dd3b213457274e75b655c1ada8836d3fb29014
neffati229 <hajer@neffati229.com>
2026-05-18 20:25:00 +0100

    Fix outputs.tf: use web array syntax

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,9 @@
+output "instance_public_ips" {
+  description = "Public IPs of EC2 instances"
+  value       = aws_instance.web[*].public_ip
+}
+
 output "instance_public_ip" {
-  value = [
-    aws_instance.web1.public_ip,
-    aws_instance.web2.public_ip,
-  ]
+  description = "Public IP of first EC2 instance"
+  value       = aws_instance.web[0].public_ip
 }
